871d5cc610a9c47cbaf1c5c977da3b03079ab89d
staaph <contact.staaph@gmail.com>
2022-05-30 17:32:34 +0200

    Initial commit

---
+++ src/components/NavbarTop.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useDark } from '@/composables/useDark';
+import Sun from '@/assets/icons/SunIcon.vue';
+import Moon from '@/assets/icons/MoonIcon.vue';
+
+const { userTheme, toggleTheme } = useDark();
+</script>
+
+<template>
+  <nav>
+    <button>
+      <Sun @click="toggleTheme" v-if="userTheme === 'dark'" />
+      <Moon v-else @click="toggleTheme" />
+    </button>
+  </nav>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NavbarTop from '@/components/NavbarTop.vue';
+</script>
+
+<template>
+  <NavbarTop />
+</template>
